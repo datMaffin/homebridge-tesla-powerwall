55ccca1bad54d7276572b9994c8eb0b1e2a3f27d
datMaffin <maffinmaffinmaffinmaffin@gmail.com>
2018-06-15 21:04:52 +0200

    Add test structure

---
+++ test/test.js
@@ -0,0 +1,49 @@
+/* eslint-env mocha */
+var assert = require('assert');
+
+describe('Utilites', function() {
+    describe('#_httpGetRequest(url, callback)', function() {
+        it('should succeed', function() {
+            assert.equal(0, 0);
+        });
+    });
+    describe('#_parseJSON(str)', function() {
+        it('should succeed', function() {
+            assert.equal(0, 0);
+        });
+    });
+    describe('ValueGetter', function() {
+        it('should succeed', function() {
+            assert.equal(0, 0);
+        });
+    });
+    describe('#reset(service, characteristic, getterByCallback, delay)', function() {
+        it('should succeed', function() {
+            assert.equal(0, 0);
+        });
+    });
+    describe('Polling', function() {
+        it('should succeed', function() {
+            assert.equal(0, 0);
+        });
+    });
+});
+describe('Accessories', function() {
+    describe('Powerwall', function() {
+        it('should succeed', function() {
+            assert.equal(0, 0);
+        });
+    });
+    describe('PowerMeter', function() {
+        it('should succeed', function() {
+            assert.equal(0, 0);
+        });
+    });
+});
+describe('Platform', function() {
+    describe('TeslaPowerwall', function() {
+        it('should succeed', function() {
+            assert.equal(0, 0);
+        });
+    });
+});
